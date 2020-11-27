25f6f3143bf4df709d9a6b7680c31e3d3d91c343
z3dc0ps <61974788+z3dc0ps@users.noreply.github.com>
2020-11-27 13:08:17 -0500

    Delete other_module.nim

--- tools/altsub/tests/modules/other_module.nim
+++
@@ -1,8 +0,0 @@
-# Extra module to test exporting multiple modules from Nim
-import ../../nimpy
-
-pyExportModule("other_module", doc="Other module docstring")
-
-proc other_proc(): int {.exportpy.} =
-  ## Other proc docstring
-  result = 9
