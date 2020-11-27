365706ecfc7cced69cb533f26f0f27e6bff4fd1c
z3dc0ps <61974788+z3dc0ps@users.noreply.github.com>
2020-11-28 00:41:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ sudo apt  install cargo
 sudo apt install python3-pip
 sudo apt-get install curl
 sudo apt-get install jq
+pip3 install -r requirements.txt
 git clone https://github.com/Edu4rdSHL/findomain.git
 cd findomain
 cargo build --release
@@ -11,5 +12,5 @@ cd ../
 go get -u github.com/tomnomnom/httprobe
 go get -u github.com/tomnomnom/assetfinder
 go get -u github.com/tomnomnom/anew
-pip3 install -r requirements.txt
+GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
 export PATH=$PATH:$(go env GOPATH)/bin
