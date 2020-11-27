d608996c9f8157f0e195e6b1790b461363e5b382
z3dc0ps <61974788+z3dc0ps@users.noreply.github.com>
2020-11-27 13:09:07 -0500

    Delete custommodulename.nim

--- tools/altsub/tests/custommodulename.nim
+++
@@ -1,9 +0,0 @@
-import ../nimpy
-
-# The module built from this file is renamed to _mycustommodulename in nimble test
-
-pyExportModule(name = "_mycustommodulename", doc = """
-This is the doc
-for my module""")
-
-proc hello(): int {.exportpy.} = 5
