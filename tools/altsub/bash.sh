2068868c2ae96974b5597c00da4c189a0583d158
z3dc0ps <61974788+z3dc0ps@users.noreply.github.com>
2020-11-27 13:07:25 -0500

    Delete bash.sh

--- tools/altsub/bash.sh
+++
@@ -1,9 +0,0 @@
-result=`python3 test1.py`
-temp_index=`echo $result | grep -b -o "server" | cut -d ":" -f1`
-index=`expr $temp_index + 10`
-
-
-server=`echo $result | cut -c$index-| cut -d "," -f1 `
-status=`echo $result  | cut -d "{" -f1`
-
-echo $server $status
